b1c6a86af2a7913e205434c7d829e19ef392f2a1
314Degrees <45397835+314Degrees@users.noreply.github.com>
2019-01-18 01:21:22 +0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -19,6 +19,7 @@ window.fn.load = function(page, id) {
 				if (document.querySelector('ons-tabbar').getActiveTabIndex() == 0) {
 					getTransaction();
 					getHistory();
+					getProfile(loginAs);
 				}
 			});
 		} else if (page == 'editCostume.html') {
@@ -441,4 +442,97 @@ function addOrder(id) {
 			}
 		}
 	});
+}
+
+function addCustomer() {
+	newCustomerFullname = $("#registerFullname").val();
+	newCustomerUsername = $("#registerUsername").val();
+	newCustomerPassword = $("#registerPassword").val();
+	newCustomerEmail = $("#registerEmail").val();
+	newCustomerPhone = $("#registerPhone").val();
+	newCustomerAddress = $("#registerAddress").val();
+
+	if (newCustomerFullname !== '' && newCustomerUsername !== '' && newCustomerPassword !== '' && newCustomerEmail !== '' && newCustomerPhone !== '' && newCustomerAddress !=='') {
+		var modal = document.getElementById('loaderOverlay');
+		modal.show();
+
+		var url = `${serviceHost}/add_customer.php?fullname=${newCustomerFullname}&username=${newCustomerUsername}&password=${newCustomerPassword}&email=${newCustomerEmail}&phone=${newCustomerPhone}&address=${newCustomerAddress}`;
+		console.log(url);
+		$.ajax({
+			url: url,
+			method: 'GET',
+			dataType: 'JSON',
+			success: function (res) {
+				var print = "";
+				if (res.query_result[0].isSuccess == "true") {
+					setTimeout(function(){
+						modal.hide();
+						document.getElementById('myNavigator').popPage();
+						ons.notification.toast('Berhasil terdaftar!', { timeout: 2000 });
+					}, 2000);
+				} else {
+					ons.notification.alert("Data gagal tersimpan!");
+				}
+			},
+			error: function (err) {
+				console.log(err);
+			}
+		});
+	} else {
+		ons.notification.alert("Lengkapi semua input!");
+	}
+}
+
+function getProfile(username) {
+	var url = `${serviceHost}/get_profile.php?username=${username}&type=customer`;
+	$.ajax({
+		url: url,
+		method: 'GET',
+		dataType: 'JSON',
+		success: function (res) {
+			var print = "";
+			var profile = res.profile;
+
+			print += `
+					<ons-card>
+						<div class="title">
+								${profile[0].username}
+							</div>
+							<ons-row>
+								<ons-col style='padding-left: 5px;' class='resultSection'>
+									<div>Username :</div>
+									<div>Email :</div>
+									<div>Phone :</div>
+									<div>Address :</div>
+								</ons-col>
+								<ons-col style='padding-left: 5px;' class='resultSection'>
+									<div class='profileUsername'>${profile[0].username}</div>
+									<div class='profileEmail'>${profile[0].email}</div>
+									<div class='profilePhone'>${profile[0].phone}</div>
+									<div class='profileAddress'>${profile[0].address}</div>
+								</ons-col>
+							</ons-row>
+							<ons-row>
+								<ons-col>
+									<ons-button modifier="large" onclick="logout()">Sign Out</ons-button>
+								</ons-col>
+							</ons-row>
+					</ons-card>
+			`;
+			document.getElementById("profileHolder").innerHTML = print;
+			document.getElementById("profileLoader").style.display = "none";
+		},
+
+		error: function (err) {
+			console.log(err);
+		}
+	});
+}
+
+function logout() {
+	mydb.transaction(function (t) {
+		t.executeSql(`DELETE FROM customerloggedin WHERE username='${loginAs}'`);
+	});
+
+	document.getElementById('myNavigator').resetToPage('login.html');
 }
